dbf8110a9eea9f95200c629a3f12f7ee327cf029
asus <atphng@hotmail.com>
2023-05-02 22:56:40 +0800

    embed multiple image and download as zip finished

--- website/static/watermark.js
+++ website/static/watermark.js
@@ -1,9 +1,10 @@
 // For basic watermark embedding page.
 const fileInput = document.getElementById("image_input"),
-  chooseImgBtn = document.querySelector(".choose-img");
+  chooseImgBtn = document.querySelector(".choose-img"),
+  batchInput = document.getElementById("image_batch"),
+  multipleImgBtn = document.querySelector(".multiple-img");
 previewImg = document.querySelector(".preview-img img");
 const embedBtn = document.querySelector(".embed");
-
 
 //Try new code to load the image uploaded
 const loadImage = () => {
@@ -13,6 +14,15 @@ const loadImage = () => {
   embedBtn.removeAttribute("hidden");
 };
 
+const loadImageBatch = () => {
+  let file = batchInput.files[0]; //getting user selected file
+  if (!file) return; //return if user hasn't selected file
+  previewImg.src = URL.createObjectURL(file); //passing file url as preview img src
+  embedBtn.removeAttribute("hidden");
+};
 
 chooseImgBtn.addEventListener("click", () => fileInput.click());
 fileInput.addEventListener("change", loadImage);
+
+multipleImgBtn.addEventListener("click", () => batchInput.click());
+batchInput.addEventListener("change", loadImageBatch)
--- website/views.py
+++ website/views.py
@@ -4,9 +4,13 @@ from pathlib import Path
 
 from PIL import Image
 from PIL import ImageFont, ImageDraw, ImageColor
-from flask import Blueprint, Flask, render_template, flash, request, jsonify, redirect, url_for, Response
+from flask import Blueprint, Flask, render_template, flash, request, jsonify, Response
+import flask
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
+import zipfile
+from os.path import basename
+
 
 from . import db
 from .models import Note, Imge
@@ -73,7 +77,7 @@ def canvas():
     return render_template("canvas.html", user=current_user, image=Path(f"static/image/{pic}"))
 
 
-def apply_watermark(raw_image, name, watermark_text, fontType, placement, color):
+def apply_watermark(raw_image, name, watermark_text, fontType, placement, color, type):
     # Guard clause to handle the error
     # Ref: https://subscription.packtpub.com/book/programming/9781788293181/6/06lvl1sec66/guard-clauses
     if (not raw_image) or not (allowed_file(name)):
@@ -132,9 +136,15 @@ def apply_watermark(raw_image, name, watermark_text, fontType, placement, color)
 
     # Saving the new image
     watermarked = Image.alpha_composite(original_image, image_with_text)
-    filename = Path(f"{Path(name).stem}-watermarked.png")
-    watermarked.save(Path('website/static/image', filename))
-    flash('Digital Watermark embedded!', category='success')
+    
+    if type == 'single':
+        filename = Path(f"{Path(name).stem}-watermarked.png")
+        watermarked.save(Path('website/static/image', filename))
+    else:
+        filename = Path(f"{Path(name).stem}.png")
+        watermarked.save(Path('website/static/image-batch', filename))
+
+    
 
     return filename
 
@@ -144,35 +154,74 @@ def apply_watermark(raw_image, name, watermark_text, fontType, placement, color)
 def watermark():
     if request.method == 'POST':
         pic = request.files['image']
-
-        if not pic:
-            pic = request.files['preview-img']
+        pics = flask.request.files.getlist('image-batch[]')
             
         watermark_text = request.form.get('watermark_text')
-        fontType = request.form.get('fontFamily')
+        fontType = request.form.get('fontFamily') 
         placement = request.form.get('placement')
         color = request.form.get("colorpicker")
-            
-        if not pic:
+        
+        # if there are no image uploaded
+        if not pic and not pics:
             flash('no pic uploaded', category='error')
             return render_template("watermark.html", user=current_user)
+        # if there are no text written
         if len(watermark_text) < 1:
             flash('no text entered', category='error')
             return render_template("watermark.html", user=current_user)
         
+        if pic:
+            filename = secure_filename(pic.filename)
+            mimetype = pic.mimetype
 
-        filename = secure_filename(pic.filename)
-        mimetype = pic.mimetype
-        check = os.path.exists("website/static/image/" + filename + "-watermarked.png")
-        if check:
-            os.path.remove("website/static/image/" + filename + "-watermarked.png")
-        else:
-            img = Imge(image=pic.read(), mimetype=mimetype, name=filename)
-            watermarked_file = apply_watermark(pic, filename, watermark_text, fontType, placement, color)
+            # see whether the picture is already exist in the local storage, if yes delete the initial image in the storage
+            check = os.path.exists("website/static/image/" + filename + "-watermarked.png")
+            if check:
+                os.path.remove("website/static/image/" + filename + "-watermarked.png")
+            else:
+                img = Imge(image=pic.read(), mimetype=mimetype, name=filename)
+                watermarked_file = apply_watermark(pic, filename, watermark_text, fontType, placement, color, 'single')
 
-            # db.session.add(img)
-            # db.session.commit()
+                # db.session.add(img)
+                # db.session.commit()
+            flash('Digital Watermark embedded!', category='success')
             return render_template("watermark.html", user=current_user, image=Path(f"static/image/{watermarked_file}"))
+
+        else:
+            for img in pics:
+                filename = secure_filename(img.filename)
+                mimetype = img.mimetype
+
+                # see whether the picture is already exist in the local storage, if yes delete the initial image in the storage
+                check = os.path.exists("website/static/image-batch/" + filename)
+                if check:
+                    os.path.remove("website/static/image-batch/" + filename)
+                else:
+                    Img = Imge(image=pic.read(), mimetype=mimetype, name=filename)
+                    watermarked_file = apply_watermark(img, filename, watermark_text, fontType, placement, color, 'batch')
+
+                    # db.session.add(Img)
+                    # db.session.commit()
+
+        with zipfile.ZipFile('website/static/watermarked.zip','w', compression= zipfile.ZIP_DEFLATED) as zip:
+            for img in pics:
+                filename = secure_filename(img.filename)
+
+                new_filename = filename.replace(' ', '_')
+
+                # Get the name and extension parts of the filename
+                name, ext = os.path.splitext(new_filename)
+
+                #Change the extension to .png
+                new_ext = ".png"
+
+                new_filename = name + new_ext
+
+                zip.write("website/static/image-batch/" + new_filename, basename("website/static/image-batch/" + new_filename))
+
+        zip.close()
+        flash('Digital Watermark embedded!', category='success')
+        return render_template("watermark.html", user=current_user, image=Path(f"static/image-batch/{watermarked_file}"), zipfile=Path(f"static/watermarked.zip"))
 
     return render_template("watermark.html", user=current_user)
 
